e004aedfc9bb251f5a02de318721bc1e9dea6ed6
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-06-04 02:00:28 +0000

    mitmproxy:10.3.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -6,25 +6,31 @@ MIRROR=https://snapshots.mitmproxy.org
 dl() {
     local ver=$1
     local os=$2
-    local archive_type=${3:-tar.gz}
-    local file=mitmproxy-${ver}-${os}.${archive_type}
-    local lfile=$DIR/$file
-    local url=$MIRROR/${ver}/${file}
+    local arch=$3
+    local archive_type=${4:-tar.gz}
+    local platform="${os}-${arch}"
+    local file="mitmproxy-${ver}-${platform}.${archive_type}"
+    local lfile="${DIR}/${file}"
 
-    if [ ! -e $lfile ]; then
+    # https://downloads.mitmproxy.org/10.3.0/mitmproxy-10.3.0-linux-x86_64.tar.gz
+    local url="${MIRROR}/${ver}/${file}"
+
+    if [ ! -e $lfile ];
+    then
         curl -sSLf -o $lfile $url
     fi
 
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $os $(sha256sum $lfile | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver linux
-    dl $ver macos
-    dl $ver windows zip
+    dl $ver linux x86_64
+    dl $ver macos arm64
+    dl $ver macos x86_64
+    dl $ver windows x86_64 zip
 }
 
-dl_ver ${1:-10.0.0}
+dl_ver ${1:-10.3.0}
